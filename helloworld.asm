02f6e4c3c19485153b008c35514cf22c777685d3
ABradRoberts <bradr555@gmail.com>
2022-11-02 20:45:24 -0500

    holy crap i did it

---
+++ helloworld.asm
@@ -0,0 +1,17 @@
+section  .data                     ; data
+msg      db  "Hello, World!", 0x0a ; String and newline character
+section  .text                     ; text segment
+global   _start
+
+_start:
+; SYSCAL: Write(1, msg, 14)
+mov eax, 4   ; put 4 into eax since write is syscall #4
+mov ebx, 1   ; put 1 into ebx since stdout is 1
+mov ecx, msg ; put the address of the string int ecx
+mov edx, 14  ; put 14 into edx since our string is 14 bytes
+int 0x80     ; call the kernel to make the system call happen
+
+; SYSCALL: exit(0)
+mov eax, 1 ; put 1 into eax since exit is syscall 1
+mov ebx, 0 ; exit with success
+int 0x80   ; do the syscall
